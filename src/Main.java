63a7e924970bb434e92776549c3ff8c613fb8502
Sumit Patidar <sumitpatidar1941@gmail.com>
2022-09-03 14:34:53 +0530

    Printing File list Repetetion bug fixed Learned good thing

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.sql.SQLOutput;
+import java.util.Arrays;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -19,76 +20,21 @@ public class Main
         UserInteraction1 display = new UserInteraction1();
         display.displayDetailsApp();
         display.displayDetailsUserIntraction();
-        Main m = new Main();
-        int input = m.optionsInput(display);
-        if(input == 1)
-        {
-//            The first option should return the current file names in ascending order. The root directory can
-//            be either empty or contain few files or folders in it
-//            Create a File object, passing the directory path to the constructor. Use the listFiles() to retrieve
-//            an array of File objects for each file in the directory, and then call the getName() method to
-//            get the filename.
-            File f =new File("F:\\Virtual_Key_For_Your_Repositories\\src");
-            File[] listOfFiles = f.listFiles();
-            int fileCounter = 0;
-            int directoryCounter = 0;
-            for(int i =1 ; i<=listOfFiles.length;i++)
-            {
-                if(listOfFiles[i].isFile())
-                {
-                    fileCounter++;
-                    System.out.println("File "+i+". "+listOfFiles[i].getName());
-                }
-                else
-                {
-                    if(listOfFiles[i].isDirectory())
-                    {
-                        directoryCounter++;
-                        System.out.println("Directory "+i+". "+listOfFiles[i].getName());
-                    }
-                }
-            }
-            System.out.println("The root directory contains "+fileCounter+"files and "+directoryCounter+"directories");
-        }
-        if(input == 2)
-        {
-
-        }
-        if(input == 3)
-        {
-            m.option3(m,display);
-        }
-    }
-
-    void option3(Main m, UserInteraction1 display)
-    {
         while(true)
         {
-            try
+            Main m = new Main();
+            int input = m.optionsInput(display);
+            if(input == 1)
             {
-                System.out.println("Are you sure :\nSelect Y for yes\nSelect N for no");
-                Scanner sc = new Scanner(System.in);
-                char conf = sc.next().trim().charAt(0);
-                if(conf == 'y' || conf == 'Y' || conf == 'n' || conf == 'N')
-                {
-                    if(conf == 'y' || conf == 'Y')
-                    {
-                        System.out.println("Thanks for using the application...\nComeback soon..");
-                        System.exit(0);
-                    }
-                    if(conf == 'n' || conf == 'N')
-                    {
-                        m.optionsInput(display);
-                    }
-                }
-                else
-                {
-                    throw new MyExceptions();
-                }
+                m.option1(m,display);
             }
-            catch (MyExceptions a)
+            if(input == 2)
             {
-                System.out.println(a);
+
+            }
+            if(input == 3)
+            {
+                m.option3(m,display);
             }
         }
     }
@@ -121,6 +67,72 @@ public class Main
                 System.out.println(a+"\n");
             }
         }
-    return i;
+        return i;
+    }
+
+
+    void option1(Main m, UserInteraction1 display)
+    {
+        File f =new File("F:\\Virtual_Key_For_Your_Repositories");
+        File[] files = f.listFiles();
+        if(files.length == 0)
+        {
+            System.out.println("No files Found in Directory");
+        }
+        else
+        {
+            int fileCounter = 0;
+            int directoryCounter = 0;
+            String[] Names = new String[files.length];
+            for(int i =1 ; i<files.length;i++)
+            {
+                if(files[i].isFile())
+                {
+                    fileCounter++;
+                    Names[i] = files[i].getName();
+                }
+                else if (files[i].isDirectory())
+                {
+                    directoryCounter++;
+                    Names[i] =files[i].getName();
+                }
+            }
+            System.out.println(Arrays.toString(Names));
+            System.out.println("\nThe root directory contains "+fileCounter+" files and "+directoryCounter+" directories\n");
+        }
+    }
+
+    void option3(Main m, UserInteraction1 display)
+    {
+        while(true)
+        {
+            try
+            {
+                System.out.println("Are you sure :\nSelect Y for yes\nSelect N for no");
+                Scanner sc = new Scanner(System.in);
+                char conf = sc.next().trim().charAt(0);
+                if(conf == 'y' || conf == 'Y' || conf == 'n' || conf == 'N')
+                {
+                    if(conf == 'y' || conf == 'Y')
+                    {
+                        System.out.println("Thanks for using the application...\nComeback soon..");
+                        System.exit(0);
+                    }
+                    else
+                    {
+                        System.out.println("\n");
+                        return;
+                    }
+                }
+                else
+                {
+                    throw new MyExceptions();
+                }
+            }
+            catch (MyExceptions a)
+            {
+                System.out.println(a);
+            }
+        }
     }
 }
