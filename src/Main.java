f253a427d1c6b50d94dacebc2755d4a9ca5a0ab0
Sumit Patidar <sumitpatidar1941@gmail.com>
2022-09-06 10:10:30 +0530

    new feature to add files with any number of lines.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
 
 public class Main
 {
+    static String fileAddress = "F:\\FilesExample" ;
     public static void main(String[] args)
     {
         UserInteraction1 display = new UserInteraction1();
@@ -12,14 +15,14 @@ public class Main
         while(true)
         {
             Main m = new Main();
-            int input = m.optionsInput(display);
+            int input = m.optionsInputMain(display);
             if(input == 1)
             {
                 m.option1(m,display);
             }
             if(input == 2)
             {
-                System.out.println("Working on it .....This will be included in next version of application\n");
+               m.option2(display);
             }
             if(input == 3)
             {
@@ -28,7 +31,7 @@ public class Main
         }
     }
 
-    int optionsInput(UserInteraction1 display)
+    int optionsInputMain(UserInteraction1 display)
     {
         int i;
         while(true)
@@ -62,7 +65,7 @@ public class Main
 
     void option1(Main m, UserInteraction1 display)
     {
-        File f =new File("F:\\New folder");
+        File f =new File(fileAddress);
         File[] files = f.listFiles();
         if(files.length == 0)
         {
@@ -132,4 +135,89 @@ public class Main
             }
         }
     }
+
+    int optionsInputBusiness(UserInteraction1 display)
+    {
+        int i;
+        while(true)
+        {
+            try
+            {
+                display.displayDetailsOption2();
+                Scanner sc = new Scanner(System.in);
+                i = sc.nextInt();
+                if(i == 1 || i == 2 || i ==3 || i==4 || i==5)
+                {
+                    break;
+                }
+                else
+                {
+                    throw new MyExceptions();
+                }
+            }
+            catch (InputMismatchException a)
+            {
+                System.out.println("Please Enter a valid input\n");
+            }
+            catch(MyExceptions a)
+            {
+                System.out.println(a+"\n");
+            }
+        }
+        return i;
+    }
+//                System.out.println("1 - Add file");
+//                System.out.println("2 - Delete file");
+//                System.out.println("3 - Search file");
+    void option2(UserInteraction1 display) {
+        Main mainBusiness = new Main();
+        while(true)
+        {
+            int a = mainBusiness.optionsInputBusiness(display);
+            switch (a) {
+                case 1 -> {
+                    System.out.println("Enter file name :" );
+                    Scanner sc = new Scanner(System.in);
+                    String fileName = sc.nextLine();
+                    String fileData="";
+                    System.out.println("Enter data for the file : Press 'Y' and enter to add");
+                    while(sc.hasNextLine())
+                    {
+                        String temp = sc.nextLine();
+                        if(temp.trim().equals("y") || temp.trim().equals("Y"))
+                        {
+                            break;
+                        }
+                        fileData = fileData +" "+ temp;
+                    }
+                    File newFile = new File(fileName);
+                    try
+                    {
+                        FileWriter writeFile = new FileWriter(fileAddress+fileName+".txt");
+                        writeFile.write(fileData);
+                        writeFile.close();
+                    }
+                    catch (IOException e)
+                    {
+                        throw new RuntimeException(e);
+                    }
+                    System.out.println("Your file has been added successfully\n");
+                }
+                case 2 -> {
+                    System.out.println("2");
+                }
+                case 3 -> {
+                    System.out.println("3");
+                }
+                case 4 -> {
+                    return;
+                }
+                case 5 -> {
+                    mainBusiness.option3(mainBusiness,display);
+                }
+            }
+        }
+
+    }
+
 }
--- src/UserInteraction1.java
+++ src/UserInteraction1.java
@@ -9,16 +9,27 @@ public class UserInteraction1
         }
         void displayDetailsUserIntraction()
         {
+                System.out.println("Welcome to LOCKED_ME.COM");
+                System.out.println("========================\n");
                 System.out.println("This is a "+"Command Line Interface"+". Please read instructions carefully and select the appropriate option");
                 System.out.println(" ");
         }
         void displayDetailsOptions()
         {
                 System.out.println("PLEASE SELECT YOUR OPTION");
-                System.out.println("1 - Display the current file names in directory");
-                System.out.println("2 - Display the user interface");
-                System.out.println("3 - Exit");
+                System.out.println("1 - Display current file names in directory");
+                System.out.println("2 - Business level operations");
+                System.out.println("3 - Close Application");
                 System.out.println(" ");
+        }
+        void displayDetailsOption2()
+        {
+                System.out.println("SELECT BUSINESS LEVEL OPERATIONS TO PERFORM ON CURRENT DIRECTORY\n");
+                System.out.println("1 - Add file");
+                System.out.println("2 - Delete file");
+                System.out.println("3 - Search file");
+                System.out.println("4 - Go Back to context menu");
+                System.out.println("5 - Close Application");
         }
 
 }
